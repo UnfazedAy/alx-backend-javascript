c2726bec0c65cb9cac480803e4e390f431e70cad
UnfazedAy <ayomidesoniyi@gmail.com>
2023-01-24 20:28:27 +0100

    Done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  // eslint-disable
   return (new Promise((resolve, reject) => {}));
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
 
   return Promise.all([img, user])
     .then((result) => console.log(result[0].body, result[1].firstName, result[1].lastName))
-    .catch(Error('Signup system offline'));
+    .catch(() => { console.log('Signup system offline'); });
 }
