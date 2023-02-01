89543554e3c248d84648b2a28d90a235615040c8
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-01 19:19:37 +0100

    Completed task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,7 +14,36 @@ interface Directors extends Teacher {
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
+// ** This works also ** //
+// const printTeacher: printTeacherFunction = ((firstName, lastName) => {
+//   console.log(`${firstName[0]}. ${ lastName}`);
+// });
 
 function printTeacher(firstName, lastName): printTeacherFunction {
   console.log(`${firstName[0]}. ${ lastName}`);
+}
+
+interface StudentClassContructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClass {
+  constructor({firstName, lastName}: StudentClassContructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return ("Currently working")
+  }
+
+  displayName(): string {
+    return this.firstName  
+  }
 }
