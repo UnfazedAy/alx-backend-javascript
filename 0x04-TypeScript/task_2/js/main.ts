dae7b834e865b78c260a96cb7443e3bbb5598e91
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-02 10:05:47 +0100

    Completed task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,3 +42,12 @@ function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === "number" && salary < 500) return new Teacher();
   return new Director();
 }
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (<Director>employee).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface) {
+  if (isDirector(employee)) console.log(employee.workDirectorTasks());
+  else console.log(employee.workTeacherTasks());
+}
