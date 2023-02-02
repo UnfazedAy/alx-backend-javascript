b4e764b9ddc4c198935aeb55879aa4a3db3261e4
UnfazedAy <ayomidesoniyi@gmail.com>
2023-02-02 20:38:08 +0100

    Modified

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -40,17 +40,25 @@ class Teacher implements TeacherInterface {
 
 function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === "number" && salary < 500) return new Teacher();
-  return new Director();
+  else return new Director();
 }
 
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
 function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
-  return (<Director>employee).workDirectorTasks !== undefined;
+  // return (<Director>employee).workDirectorTasks !== undefined;
+  return (employee instanceof Director);
 }
 
 function executeWork(employee: DirectorInterface | TeacherInterface) {
-  if (isDirector(employee)) console.log(employee.workDirectorTasks());
-  else console.log(employee.workTeacherTasks());
+  if (isDirector(employee)) return (employee.workDirectorTasks());
+  else return (employee.workTeacherTasks());
 }
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
 
 type Subjects = "Math" | "History";
 
