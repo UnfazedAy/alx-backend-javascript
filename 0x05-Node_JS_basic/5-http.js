1b7b9bb72bb18f01d1b7344beed1bae9a7e6db4c
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-21 17:31:19 +0100

    Not enjoying this task at all

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,35 +1,61 @@
 const fs = require('fs');
 const http = require('http');
-const countStudents = require('./3-read_file_async');
 
 const { argv } = process;
 const port = 1245;
 const hostname = '127.0.0.1';
+const filePath = argv[2];
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const dataArray = data.trim().split('\n').map((row) => row.split(','));
+      const students = dataArray.slice(1);
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {}; // stores the count and list
+      students.forEach((student) => {
+        const field = student[3]; // Gets the fields of the student
+        if (!fields[field]) {
+          fields[field] = {
+            count: 0,
+            names: [],
+          };
+        }
+        fields[field].count += 1;
+        fields[field].names.push(student[0]);
+      });
+
+      // eslint-disable-next-line guard-for-in
+      for (const field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    const filePath = argv[2];
-    countStudents(filePath)
-      .then((data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.write('This is the list of our students\n');
-        res.end(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Internal Server Error');
-      });
-  } else {
-    res.statusCode = 404;
+  }
+  if (req.url === '/students') {
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found\n');
+    res.write('This is the list of our students\n');
+    countStudents(filePath).then(() => {
+      res.end();
+    }).catch((error) => {
+      res.statusCode = 500;
+      res.write(`${error}\n`);
+      res.end();
+    });
   }
 });
 
