ec237ef56d991860ed70b5fb42646f53037d6ef1
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-21 18:09:20 +0100

    Done

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const port = 1245;
+const hostname = '127.0.0.1';
 const app = express();
 
 app.get('/', (req, res) => {
-  app.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-app.listen(port);
+app.listen(port, hostname);
 
 module.exports = app;
